e5d1d20c162ca7b9872d962110f3f1c72e5f3991
Joseph Cabusas <jdcabusas201@gmail.com>
2020-02-29 07:57:57 +0800

    Add files via upload

---
+++ Model/Applicant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Applicant_List.Model
+{
+    public class Applicant
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Exp { get; set; }
+        public string Degree { get; set; }
+        public string Major { get; set; }
+    }
+}
---
+++ Pages/ApplicantList/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Applicant_List.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Applicant_List
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Applicant> Applicants { get; set; }
+
+
+        public async Task OnGet()
+        {
+            Applicants = await _db.Applicant.ToListAsync();
+        }
+    }
+}
